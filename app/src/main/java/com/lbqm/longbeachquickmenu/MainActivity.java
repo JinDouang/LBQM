20947d23f62a31192d9f62d28e6dc14970c60fb6
JinDouang <jinhda.douangphrachanh@epitech.eu>
2018-03-08 19:14:44 -0800

    Fixed some issues on the design
    Work in progress to finish the design

--- app/src/main/java/com/lbqm/longbeachquickmenu/MainActivity.java
+++ app/src/main/java/com/lbqm/longbeachquickmenu/MainActivity.java
@@ -4,18 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    /* Init spinner button */
+    Spinner sp;
+
+    /* List of category */
+    String categories[] = {"Breakfast", "Lunch", "Dinner"};
+    ArrayAdapter<String> adapter;
+
+
+    /* data variable that will be used to obtain category value */
+    String dataCategory= "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
         /*
         Set button for each menu in order to redirect to their own view.
@@ -35,6 +46,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         */
         setCalendar();
 
+        /*
+        * set button spinner to switch category
+        * */
+        setSpinner();
+
+    }
+
+    /*
+    Method that will make view category
+    */
+    private void setSpinner() {
+        sp = findViewById(R.id.spinner);
+        adapter = new ArrayAdapter<>(this, R.layout.spinner_item, categories);
+        sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        dataCategory = "Breakfast";
+                        System.out.println("Category: " + dataCategory);
+                        break;
+                    case 1:
+                        dataCategory = "Lunch";
+                        System.out.println("Category: " + dataCategory);
+                        break;
+                    case 2:
+                        dataCategory = "Dinner";
+                        System.out.println("Category: " + dataCategory);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     /*
@@ -59,9 +108,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void setCalendar() {
-        FrameLayout bottomNav = findViewById(R.id.bottomNav);
-        LinearLayout bottomContent = bottomNav.findViewById(R.id.bottomContent);
-        Button Calendar = bottomContent.findViewById(R.id.CalendarViewButton);
+        Button Calendar = findViewById(R.id.CalendarViewButton);
         Calendar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, CalendarActivity.class));
